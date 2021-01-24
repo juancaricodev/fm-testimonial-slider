c2e455c3fe134602cd6cf68e52327a58f1b5e181
Juan Camilo Rico <juancarico92@gmail.com>
2021-01-23 21:41:52 -0500

    feat: js/main.js added to project and slider controls tested

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const prev = document.getElementById('prevBtn')
+const next = document.getElementById('nextBtn')
+
+prev.addEventListener('click', () => {
+  console.log('prev button working')
+})
+
+next.addEventListener('click', () => {
+  console.log('next button working')
+})
